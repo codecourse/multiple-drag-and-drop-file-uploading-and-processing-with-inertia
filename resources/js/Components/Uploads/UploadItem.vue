939a68dae268b08307125bf584c6ae20da151541
Alex Garrett-Smith <alex@codecourse.com>
2023-11-21 09:11:12 +0000

    Episode 03

---
+++ resources/js/Components/Uploads/UploadItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    upload: Object
+})
+</script>
+
+<template>
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900">
+        {{ upload }}
+    </div>
+</div>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,19 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue'
+import UploadItem from '@/Components/Uploads/UploadItem.vue'
+
+const uploads = ref([])
+
+const handleDroppedFiles = (files) => {
+    Array.from(files).forEach((file) => {
+        uploads.value.unshift({
+            id: 1,
+            title: file.name,
+        })
+    })
+}
 </script>
 
 <template>
@@ -12,14 +25,18 @@ import { Head } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <form class="p-6 text-gray-900">
                         <label for="file" class="flex items-center justify-center w-full h-44 bg-gray-100 border-2 border-dashed border-gray-200 text-gray-500 font-medium">
                             Drop or click to upload files
                         </label>
-                        <input type="file" id="file" class="sr-only" multiple>
+                        <input type="file" id="file" class="sr-only" multiple v-on:change="handleDroppedFiles($event.target.files)">
                     </form>
+                </div>
+
+                <div class="space-y-3">
+                    <UploadItem v-for="upload in uploads" :key="upload.id" :upload="upload" />
                 </div>
             </div>
         </div>
