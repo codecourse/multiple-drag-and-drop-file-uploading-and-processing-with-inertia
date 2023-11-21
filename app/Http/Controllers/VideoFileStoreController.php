8da9ea8aa08ace6495989cab90dc8923920aa0c1
Alex Garrett-Smith <alex@codecourse.com>
2023-11-21 10:40:04 +0000

    Episode 08

--- app/Http/Controllers/VideoFileStoreController.php
+++ app/Http/Controllers/VideoFileStoreController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Pion\Laravel\ChunkUpload\Handler\ContentRangeUploadHandler;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class VideoFileStoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request, Video $video)
     {
-        //
+        $receiver = new FileReceiver(
+            UploadedFile::fake()->createWithContent('file', $request->getContent()),
+            $request,
+            ContentRangeUploadHandler::class,
+        );
+
+        $save = $receiver->receive();
+
+        if ($save->isFinished()) {
+            return $this->storeAndAttachFile($save->getFile(), $video);
+        }
+
+        $save->handler();
+    }
+
+    protected function storeAndAttachFile(UploadedFile $file, Video $video)
+    {
+        $video->update([
+            'video_path' => $file->storeAs('videos', Str::uuid(), 'public')
+        ]);
     }
 }
