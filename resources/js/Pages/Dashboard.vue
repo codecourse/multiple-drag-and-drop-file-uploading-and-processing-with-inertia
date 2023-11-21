2e169b3b1b778bd18dbec4aef52c3dfbd08ee61b
Alex Garrett-Smith <alex@codecourse.com>
2023-11-21 11:16:10 +0000

    Episode 12

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,6 +7,7 @@ import axios from 'axios'
 import { createUpload } from '@mux/upchunk'
 
 const uploads = ref([])
+const filesBeingDragged = ref(false)
 
 const getUploadById = (id) => {
     return uploads.value.find((upload) => upload.id === id)
@@ -83,7 +84,7 @@ const handleDroppedFiles = (files) => {
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <form class="p-6 text-gray-900">
-                        <label for="file" class="flex items-center justify-center w-full h-44 bg-gray-100 border-2 border-dashed border-gray-200 text-gray-500 font-medium">
+                        <label for="file" class="flex items-center justify-center w-full h-44 bg-gray-100 border-2 border-dashed border-gray-200 text-gray-500 font-medium" v-on:dragover.prevent="filesBeingDragged = true" v-on:dragleave.prevent="filesBeingDragged = false" v-on:drop.prevent="handleDroppedFiles($event.dataTransfer.files); filesBeingDragged = false;" :class="{ 'bg-gray-200 border-gray-300': filesBeingDragged }">
                             Drop or click to upload files
                         </label>
                         <input type="file" id="file" class="sr-only" multiple v-on:change="handleDroppedFiles($event.target.files)">
